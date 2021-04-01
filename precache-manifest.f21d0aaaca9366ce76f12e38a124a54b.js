6808b2201bf10d0f1158bbca36752fe44b10b84d
mnkin <2664306866@qq.com>
2021-04-01 17:39:45 +0800

    deploy

---
+++ precache-manifest.f21d0aaaca9366ce76f12e38a124a54b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a927c2d6f99bb93b871",
+    "url": "/Love-books-website/css/app.338df94d.css"
+  },
+  {
+    "revision": "3ae46b92c7063c9f4203123b89e6df5c",
+    "url": "/Love-books-website/index.html"
+  },
+  {
+    "revision": "9a927c2d6f99bb93b871",
+    "url": "/Love-books-website/js/app.41525fc6.js"
+  },
+  {
+    "revision": "8e1de1274300599b056f",
+    "url": "/Love-books-website/js/chunk-vendors.cca51093.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/Love-books-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Love-books-website/robots.txt"
+  }
+]);
